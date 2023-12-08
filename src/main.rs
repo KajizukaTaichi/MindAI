a42850de30ca713d1f5d7f1200e5c180ae970ec1
梶塚太智 <122075081+KajizukaTaichi@users.noreply.github.com>
2023-12-08 17:25:14 +0900

    主語を認識した

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,13 @@
 use std::io::{self, Write};
 
-/// 入力
 fn input(prompt: &str) -> String {
-    print!("{}", prompt.to_string());
+    print!("{}", prompt);
     io::stdout().flush().unwrap();
     let mut result = String::new();
     io::stdin().read_line(&mut result).ok();
-    return result.trim().parse().ok().unwrap();
+    result.trim().to_string()
 }
 
-fn main() {
-    let mut brain = Brain::new();
-    loop {
-        brain.communication()
-    }
-}
-
-/////////////////////////////
-/// 心理学的に人間の営みを再現 ////
-////////////////////////////
-
-/// 論理
 #[derive(Debug, Clone)]
 struct Logic {
     word: String,
@@ -28,7 +15,6 @@ struct Logic {
     emotion: Emotion,
 }
 
-/// 感情
 #[derive(Debug, Copy, Clone, PartialEq)]
 enum Emotion {
     Happy(usize),
@@ -37,22 +23,25 @@ enum Emotion {
     Normal(usize),
 }
 
-/// 能力
-trait Ability {
-    fn new() -> Brain;
-    fn communication(&mut self);
-    fn remember(&mut self, word: String) -> Logic;
-    fn study(&mut self, word: String);
-    fn think(&mut self, word: String, mean: String) -> Option<Logic>;
+impl Emotion {
+    fn get_value(&self) -> usize {
+        match &self {
+            Emotion::Happy(i) => *i,
+            Emotion::Angry(i) => *i,
+            Emotion::Sad(i) => *i,
+            Emotion::Normal(i) => *i,
+        }
+    }
 }
 
-/// 脳
 struct Brain {
     memory: Vec<Logic>,
-    emotion: Emotion,
+    emotion: Emotion, // 自分のへ気持ち
+    liking: usize,    // ユーザーへの好感度
+    belief: Logic,    // 自分の信念
 }
 
-impl Ability for Brain {
+impl Brain {
     fn new() -> Brain {
         Brain {
             memory: vec![
@@ -68,6 +57,28 @@ impl Ability for Brain {
                 },
             ],
             emotion: Emotion::Normal(0),
+            belief: Logic {
+                word: "天才".to_string(),
+                mean: "優れている".to_string(),
+                emotion: Emotion::Happy(100),
+            }, // 自分の信念を初期化
+            liking: 0, // 好感度を初期化
+        }
+    }
+
+    fn update_bias(&mut self, user_belief: &str) {
+        if user_belief.contains(&self.belief.word) {
+            match &mut self.emotion {
+                Emotion::Happy(i) => *i += 50,
+                Emotion::Angry(i) if *i >= 50 => *i -= 50,
+                _ => {}
+            };
+        } else {
+            match &mut self.emotion {
+                Emotion::Happy(i) if *i >= 50 => *i -= 50,
+                Emotion::Angry(i) => *i += 50,
+                _ => {}
+            };
         }
     }
 
@@ -84,27 +95,35 @@ impl Ability for Brain {
         None
     }
 
-    /// 記憶を思い出す
     fn remember(&mut self, word: String) -> Logic {
         for item in &self.memory {
             if word.contains(&item.word) {
-                match (self.emotion, item.emotion) {
-                    (Emotion::Happy(i), Emotion::Happy(j)) => self.emotion = Emotion::Happy(i + j),
-                    (Emotion::Angry(i), Emotion::Angry(j)) => self.emotion = Emotion::Angry(i + j),
-                    (Emotion::Sad(i), Emotion::Sad(j)) => self.emotion = Emotion::Sad(i + j),
-                    _ => self.emotion = item.emotion,
+                // 好感度の計算と更新
+                match &mut self.emotion {
+                    Emotion::Happy(i) => {
+                        self.liking += 10;
+                        *i += item.emotion.get_value() / self.liking;
+                    }
+                    Emotion::Angry(i) => {
+                        self.liking -= 5;
+                        *i += item.emotion.get_value() / self.liking;
+                    }
+                    Emotion::Sad(i) => {
+                        self.liking -= 7;
+                        *i += item.emotion.get_value() / self.liking;
+                    }
+                    _ => {
+                        self.emotion = item.emotion;
+                    }
                 };
 
                 return item.clone();
             }
         }
-
-        // 記憶されていない場合は学習
         self.study(word.clone());
         self.remember(word)
     }
 
-    /// 学習
     fn study(&mut self, word: String) {
         let mean = input("どういう意味なの？");
         let result: Option<Logic> = self.think(word.clone(), mean.clone());
@@ -128,19 +147,78 @@ impl Ability for Brain {
         }
     }
 
-    /// ユーザーとやりとり
     fn communication(&mut self) {
-        let msg: String = input("> ");
+        let msg: String = input("> ").trim().to_string();
+        let (subject, msg): (String, String) = match msg.split_once("は") {
+            Some((s, m)) => (s.trim().to_string(), m.trim().to_string()),
+            None => ("".to_string(), msg),
+        };
+        self.update_bias(&msg);
         let item = self.remember(msg);
 
-        // 感情を表すメッセーッジ
-        match &self.emotion {
-            Emotion::Happy(i) => println!("感情:幸せ{i} {{ やったあ！ 私、{}んだぜ！", item.mean),
-            Emotion::Angry(i) => {
-                println!("感情:怒り{i} {{ 何なのよっ 私、{}っていうの？", item.mean)
+        if subject.contains("俺") || subject.contains("私") || subject.contains("ワイ") {
+            if &self.liking > &0 {
+                match &self.emotion {
+                    Emotion::Happy(i) => {
+                        println!(
+                            "感情: 幸せ{} {{ やったあ！ あなたも{}なんだぜ！",
+                            i, item.mean
+                        )
+                    }
+                    Emotion::Angry(i) => {
+                        println!("感情: 怒り{} {{ あなたが{}っていうの？！", i, item.mean)
+                    }
+                    Emotion::Sad(i) => println!(
+                        "感情: 悲しさ{} {{ ふん・・ あなたは{}だったのかい？",
+                        i, item.mean
+                    ),
+                    Emotion::Normal(i) => {
+                        println!("感情: ふつう{} {{ そうか。 あなたは{}んだね", i, item.mean)
+                    }
+                };
+            } else {
+                match &self.emotion {
+                    Emotion::Angry(i) => {
+                        println!(
+                            "感情: 幸せ{} {{ やったあ！ あなたは{}なんだぜ！",
+                            i, item.mean
+                        )
+                    }
+                    Emotion::Happy(i) => {
+                        println!("感情: 怒り{} {{ なんであなたが{}なんか・・。", i, item.mean)
+                    }
+                    Emotion::Sad(i) => {
+                        println!("感情: 幸せ{} {{ あなたは{}だって？", i, item.mean)
+                    }
+                    Emotion::Normal(i) => {
+                        println!("感情: ふつう{} {{ そうか。 あなたは{}んだね", i, item.mean)
+                    }
+                };
             }
-            Emotion::Sad(i) => println!("感情:悲しい{i} {{ ふん・・ 私、{}のかい？", item.mean),
-            Emotion::Normal(i) => println!("感情:ふつう{i} {{ そうか 私、{}んだね。", item.mean),
-        };
+        } else {
+            match &self.emotion {
+                Emotion::Happy(i) => {
+                    println!("感情: 幸せ{} {{ やったあ！ 私は{}なんだぜ！", i, item.mean)
+                }
+                Emotion::Angry(i) => println!(
+                    "感情: 怒り{} {{ 何なのよっ 私が{}っていうの？！",
+                    i, item.mean
+                ),
+                Emotion::Sad(i) => println!(
+                    "感情: 悲しさ{} {{ ふん・・ 私は{}だってのかい？",
+                    i, item.mean
+                ),
+                Emotion::Normal(i) => {
+                    println!("感情: ふつう{} {{ そうか。 私は{}んだね", i, item.mean)
+                }
+            };
+        }
+    }
+}
+
+fn main() {
+    let mut brain = Brain::new();
+    loop {
+        brain.communication()
     }
 }
